9e29ab99d509c5e5fefe433728d7da2340fcfcd0
philippineglobaloutsourcing <philippineoutsourcing@gmail.com>
2015-12-16 00:35:22 +0800

    Updated Readme and Packages

--- config/app.php
+++ config/app.php
@@ -145,6 +145,28 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        /**
+         * @package third-party
+         */
+
+        Caffeinated\Modules\ModulesServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
+        Roumen\Feed\FeedServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
+
+
+        // App\Packages\Jsonize\JsonizeServiceProvider::class,
+        // App\Packages\Adify\AdifyServiceProvider::class,
+        // App\Packages\Flashify\FlashifyServiceProvider::class,
+        // App\Packages\Menufy\MenufyServiceProvider::class,
+        // App\Packages\SideMenufy\SideMenufyServiceProvider::class,
+        // App\Packages\Shortly\ShortlyServiceProvider::class,
+        // App\Packages\FeedMe\FeedMeServiceProvider::class,
+        // App\Packages\Breadly\BreadlyServiceProvider::class,
+
+
     ],
 
     /*
@@ -160,38 +182,59 @@ return [
 
     'aliases' => [
 
-        'App'       => Illuminate\Support\Facades\App::class,
-        'Artisan'   => Illuminate\Support\Facades\Artisan::class,
-        'Auth'      => Illuminate\Support\Facades\Auth::class,
-        'Blade'     => Illuminate\Support\Facades\Blade::class,
-        'Bus'       => Illuminate\Support\Facades\Bus::class,
-        'Cache'     => Illuminate\Support\Facades\Cache::class,
-        'Config'    => Illuminate\Support\Facades\Config::class,
-        'Cookie'    => Illuminate\Support\Facades\Cookie::class,
-        'Crypt'     => Illuminate\Support\Facades\Crypt::class,
-        'DB'        => Illuminate\Support\Facades\DB::class,
-        'Eloquent'  => Illuminate\Database\Eloquent\Model::class,
-        'Event'     => Illuminate\Support\Facades\Event::class,
-        'File'      => Illuminate\Support\Facades\File::class,
-        'Gate'      => Illuminate\Support\Facades\Gate::class,
-        'Hash'      => Illuminate\Support\Facades\Hash::class,
-        'Input'     => Illuminate\Support\Facades\Input::class,
-        'Lang'      => Illuminate\Support\Facades\Lang::class,
-        'Log'       => Illuminate\Support\Facades\Log::class,
-        'Mail'      => Illuminate\Support\Facades\Mail::class,
-        'Password'  => Illuminate\Support\Facades\Password::class,
-        'Queue'     => Illuminate\Support\Facades\Queue::class,
-        'Redirect'  => Illuminate\Support\Facades\Redirect::class,
-        'Redis'     => Illuminate\Support\Facades\Redis::class,
-        'Request'   => Illuminate\Support\Facades\Request::class,
-        'Response'  => Illuminate\Support\Facades\Response::class,
-        'Route'     => Illuminate\Support\Facades\Route::class,
-        'Schema'    => Illuminate\Support\Facades\Schema::class,
-        'Session'   => Illuminate\Support\Facades\Session::class,
-        'Storage'   => Illuminate\Support\Facades\Storage::class,
-        'URL'       => Illuminate\Support\Facades\URL::class,
-        'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View'      => Illuminate\Support\Facades\View::class,
+        'App'        => Illuminate\Support\Facades\App::class,
+        'Artisan'    => Illuminate\Support\Facades\Artisan::class,
+        'Auth'       => Illuminate\Support\Facades\Auth::class,
+        'Blade'      => Illuminate\Support\Facades\Blade::class,
+        'Bus'        => Illuminate\Support\Facades\Bus::class,
+        'Cache'      => Illuminate\Support\Facades\Cache::class,
+        'Config'     => Illuminate\Support\Facades\Config::class,
+        'Cookie'     => Illuminate\Support\Facades\Cookie::class,
+        'Crypt'      => Illuminate\Support\Facades\Crypt::class,
+        'DB'         => Illuminate\Support\Facades\DB::class,
+        'Eloquent'   => Illuminate\Database\Eloquent\Model::class,
+        'Event'      => Illuminate\Support\Facades\Event::class,
+        'File'       => Illuminate\Support\Facades\File::class,
+        'Gate'       => Illuminate\Support\Facades\Gate::class,
+        'Hash'       => Illuminate\Support\Facades\Hash::class,
+        'Input'      => Illuminate\Support\Facades\Input::class,
+        'Lang'       => Illuminate\Support\Facades\Lang::class,
+        'Log'        => Illuminate\Support\Facades\Log::class,
+        'Mail'       => Illuminate\Support\Facades\Mail::class,
+        'Password'   => Illuminate\Support\Facades\Password::class,
+        'Queue'      => Illuminate\Support\Facades\Queue::class,
+        'Redirect'   => Illuminate\Support\Facades\Redirect::class,
+        'Redis'      => Illuminate\Support\Facades\Redis::class,
+        'Request'    => Illuminate\Support\Facades\Request::class,
+        'Response'   => Illuminate\Support\Facades\Response::class,
+        'Route'      => Illuminate\Support\Facades\Route::class,
+        'Schema'     => Illuminate\Support\Facades\Schema::class,
+        'Session'    => Illuminate\Support\Facades\Session::class,
+        'Storage'    => Illuminate\Support\Facades\Storage::class,
+        'URL'        => Illuminate\Support\Facades\URL::class,
+        'Validator'  => Illuminate\Support\Facades\Validator::class,
+        'View'       => Illuminate\Support\Facades\View::class,
+
+        /* third-party fascade */
+        'Module'     => Caffeinated\Modules\Facades\Module::class,
+        'Form'       => Collective\Html\FormFacade::class,
+        'Html'       => Collective\Html\HtmlFacade::class,
+        'Image'      => Intervention\Image\Facades\Image::class,
+        'Feed'       => Roumen\Feed\Facades\Feed::class,
+        'Excel'      => Maatwebsite\Excel\Facades\Excel::class,
+
+
+        // 'Module'     => Caffeinated\Modules\Facades\Module::class,
+        // 'Jsonize'    => App\Packages\Jsonize\JsonizeService::class,
+        // 'Adify'      => App\Packages\Adify\AdifyService::class,
+        // 'Flashify'   => App\Packages\Flashify\FlashifyService::class,
+        // 'Menufy'     => App\Packages\Menufy\MenufyService::class,
+        // 'SideMenufy' => App\Packages\SideMenufy\SideMenufyService::class,
+        // 'FeedMe'     => App\Packages\FeedMe\FeedMeService::class,
+        // 'Breadly'    => App\Packages\Breadly\BreadlyService::class,
+        // 'Shortly'    => App\Packages\Shortly\ShortlyService::class,
+
+
 
     ],
 
